e92704a1e97c13f24f39d4d0e6c8deed4142e7f0
Sue <choi.soyoung@gmail.com>
2023-12-19 22:16:24 -0800

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main >
+      <div>
         <p>
           Camping Reservations
         </p>
